f5c0a790717a31b07c381c3d70544d1f9b612b28
Everton Figueirôa <evertonbpg@gmail.com>
2022-09-14 23:44:59 -0300

    Create santander.java

---
+++ santander.java
@@ -0,0 +1,32 @@
+
+public class santander {
+        public static void main(String[] args) {
+
+        int nota = 90;
+        String graduacao;
+
+        if (nota <= 60) {
+            graduacao = "D";
+        } else if (nota < 70) {
+            graduacao = "C";
+        } else if (nota < 80) {
+            graduacao = "B";
+        } else {
+            graduacao = "A";
+        }
+
+        switch (graduacao) {
+            case "A":
+            case "B":
+                System.out.println("Aprovado!");
+                break;
+            case "C":
+            case "D":
+                System.out.println("Reprovado!");
+                break;
+            default:
+                System.out.println("Graduação invalida!");
+        }
+
+    }
+}
