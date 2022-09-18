6b272e404939f90d834341c9016f4c8beffbff60
Everton Figueirôa <evertonbpg@gmail.com>
2022-09-18 15:09:33 -0300

    Update Ex05.java

--- Ex05.java
+++ Ex05.java
@@ -1,6 +1,13 @@
 public class Ex05 {
+
     public static void main(String[] args) {
-        
+        int resultado = soma(2, 4);
+        System.out.println(resultado);
+    
+    }
+
+    public static int soma(int a, int b) {
+        return a + b;
         
     }
 }
